74f1fa8e2216fe2fec1ba2e454e039dd3693bbc7
Isak Larsson <isak.valter.larsson@gmail.com>
2025-04-08 14:13:04 +0200

    Add simple farm plot

---
+++ farming/farm_plot.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+@onready var plant: Node3D = $Wheat
+
+@export var growth_level: float = 0.0 # From 0-1
+@export var planted = false
+var growth_per_day: float = 0.2 
+
+func _ready() -> void:
+	sow()
+
+func _process(delta: float) -> void:
+	if planted:
+		plant.scale.y = growth_level
+	else:
+		plant.visible = false
+		
+func growth_tick():
+	if planted:
+		growth_level += growth_per_day
+		clamp(growth_level, 0.0, 1.0)
+
+func sow():
+	planted = true
+	plant.visible = true
+
+
+func _on_new_day(day: int) -> void:
+	growth_tick()
