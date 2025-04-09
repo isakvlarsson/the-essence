fa22e2eeb1b50d3af298462a67415e34e7e16c3f
Markus Wessén <mwesse@kth.se>
2025-04-09 15:36:53 +0200

    Added sprites and dashing function

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,15 +1,31 @@
 extends CharacterBody2D
 
 var speed = 400
+var canDash = true
+var dashing = false
+var dashSpeed = 1200
+var dashDirection = Vector2.ZERO
 
 func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * speed
+	if not dashing:
+		var input_direction = Input.get_vector("left", "right", "up", "down")
+		velocity = input_direction * speed
+		dashDirection = input_direction
 
 func _input(event):
 	if Input.is_action_pressed("whack"):
 		get_node("AnimatedSprite2D").play("whack")
 		
+func dash():
+	if Input.is_action_just_pressed("dash") and canDash and dashDirection != Vector2.ZERO:
+		velocity = dashDirection.normalized()*dashSpeed
+		canDash = false
+		dashing = true
+		await get_tree().create_timer(0.2).timeout
+		canDash = true
+		dashing = false
+		
 func _physics_process(delta):
 	get_input()
+	dash()
 	move_and_slide()
