5c5bbf5b0ba9332526dbb9f6074e526d980e2bd2
Isak Larsson <isak.valter.larsson@gmail.com>
2025-04-10 14:17:11 +0200

    Add goblin spawning

--- day_night_handler.gd
+++ day_night_handler.gd
@@ -3,6 +3,8 @@ extends Node2D
 signal new_hour(hour: int)
 signal new_day(day: int)
 
+@onready var goblin_scene = preload("res://creatures/goblin/goblin.tscn")
+
 const seconds_per_hour: float = 1 # Real-world seconds per game-hour
 var seconds_since_last_hour: float = 0
 var hour = 0
@@ -22,7 +24,22 @@ func _process(delta: float) -> void:
 			get_tree().call_group("farm_plot", "growth_tick")
 
 		new_hour.emit(hour) 
+		if hour == 0:
+			spawn_goblins()
 		
+
+func spawn_goblins():
+	var spawn_points: Array[Node] = get_tree().get_nodes_in_group("creature_spawn_point")
+	var farm_plots: Array[Node] = get_tree().get_nodes_in_group("farm_plot")
+	
+	if farm_plots.size() < 0:
+		pass
+		
+	for i in range(0, 5):
+		var goblin: CharacterBody2D = goblin_scene.instantiate()
+		goblin.target_to_chase = farm_plots.pick_random()
+		goblin.position = spawn_points.pick_random().position
+		get_tree().root.add_child(goblin)
 		
 		
 
