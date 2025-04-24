33e0077966ba55b1eedfbdedea4bcc336702133f
Markus Wessén <mwesse@kth.se>
2025-04-24 21:45:06 +0200

    Added able to place benches and improved goblins

--- farming/farm_plot.gd
+++ farming/farm_plot.gd
@@ -1,13 +1,21 @@
 extends Node2D
 
 @onready var plant: Node2D = $Plant
+@onready var eat_timer := $Timer
 
 @export var growth_level: float = 0.0 # From 0-1
 @export var planted = false
 var growth_per_day: float = 0.2 
+var min_size: float = 0.2
+var eating_creatures := []
 
 func _ready() -> void:
 	sow()
+	eat_timer = Timer.new()
+	eat_timer.wait_time = 2.0
+	eat_timer.one_shot = false
+	add_child(eat_timer)
+	eat_timer.connect("timeout", Callable(self, "_on_eat_timeout"))
 
 func _process(delta: float) -> void:
 	if planted:
@@ -23,7 +31,6 @@ func growth_tick():
 func sow():
 	planted = true
 	plant.visible = true
-
 
 func _on_new_day(day: int) -> void:
 	growth_tick()
