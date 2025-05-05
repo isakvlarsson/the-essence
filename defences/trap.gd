82be11e5a57585b66a8b5d2e552d4acb914444be
Markus Wessén <mwesse@kth.se>
2025-05-05 13:00:24 +0200

    Traps now have a cooldown after catching a goblin

--- defences/trap.gd
+++ defences/trap.gd
@@ -1,8 +1,11 @@
 extends Node2D
+var readyToTrap = true
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Creature"):
+	if body.is_in_group("Creature") and readyToTrap:
+		readyToTrap = false
 		body.queue_free()
 		$TrapArea/TrapSprite.frame = 1
 		await get_tree().create_timer(5).timeout 
 		$TrapArea/TrapSprite.frame = 0
+		readyToTrap = true
